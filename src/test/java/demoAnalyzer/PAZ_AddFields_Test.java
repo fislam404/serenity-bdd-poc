10c47e256835fb369b727342bfe84ba5f32c5165
Fahad Islam <fislam@pentaho.com>
2017-12-01 09:56:53 -0500

    commented out.

--- src/test/java/demoAnalyzer/PAZ_AddFields_Test.java
+++ src/test/java/demoAnalyzer/PAZ_AddFields_Test.java
@@ -1,45 +1,45 @@
-package demoAnalyzer;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.openqa.selenium.WebDriver;
-
-import feature.step.definition.AddFieldsStepDefinition;
-import feature.step.definition.CreateAnalyzerReportStepDefinition;
-import feature.step.definition.LoginStepDefinition;
-import net.serenitybdd.junit.runners.SerenityRunner;
-import net.thucydides.core.annotations.Managed;
-import net.thucydides.core.annotations.Steps;
-
-
-@RunWith(SerenityRunner.class)
-public class PAZ_AddFields_Test {
-  
-  @Steps
-  LoginStepDefinition loginStepDefinition;
-  @Steps
-  CreateAnalyzerReportStepDefinition createAnalyzerReportStepDefinition;
-  @Steps
-  AddFieldsStepDefinition addFieldsStepDefinition;
-
-  @Managed
-  WebDriver browser;
-  
-  @Test
-  public void logInPUC(){
-    loginStepDefinition.admin_is_on_the_loginpage();
-    loginStepDefinition.admin_enters_valid_username();
-    loginStepDefinition.admin_enters_valid_password();
-    loginStepDefinition.admin_press_Login();
-    loginStepDefinition.admin_should_successfully_logged_in();
-    createAnalyzerReportStepDefinition.admin_choose_new_Analysis_report();
-    createAnalyzerReportStepDefinition.choose_data_source();
-    addFieldsStepDefinition.admin_double_clicks_on_the_field();
-    }
-  
+//package demoAnalyzer;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.openqa.selenium.WebDriver;
+//
+//import feature.step.definition.AddFieldsStepDefinition;
+//import feature.step.definition.CreateAnalyzerReportStepDefinition;
+//import feature.step.definition.LoginStepDefinition;
+//import net.serenitybdd.junit.runners.SerenityRunner;
+//import net.thucydides.core.annotations.Managed;
+//import net.thucydides.core.annotations.Steps;
+//
+//
+//@RunWith(SerenityRunner.class)
+//public class PAZ_AddFields_Test {
+//  
+//  @Steps
+//  LoginStepDefinition loginStepDefinition;
+//  @Steps
+//  CreateAnalyzerReportStepDefinition createAnalyzerReportStepDefinition;
+//  @Steps
+//  AddFieldsStepDefinition addFieldsStepDefinition;
+//
+//  @Managed
+//  WebDriver browser;
+//  
+//  @Test
+//  public void logInPUC(){
+//    loginStepDefinition.admin_is_on_the_loginpage();
+//    loginStepDefinition.admin_enters_valid_username();
+//    loginStepDefinition.admin_enters_valid_password();
+//    loginStepDefinition.admin_press_Login();
+//    loginStepDefinition.admin_should_successfully_logged_in();
+//    createAnalyzerReportStepDefinition.admin_choose_new_Analysis_report();
+//    createAnalyzerReportStepDefinition.choose_data_source();
+//    addFieldsStepDefinition.admin_double_clicks_on_the_field();
+//    }
+//  
 //  @Test
 //  public void createAnalyzerReport(){
 //    createAnalyzerReportStepDefinition.admin_is_o_the_file_menu();
 //  }
-
-}
+//
+//}
--- src/test/java/feature/step/definition/CreateAnalyzerReportStepDefinition.java
+++ src/test/java/feature/step/definition/CreateAnalyzerReportStepDefinition.java
@@ -1,39 +1,39 @@
-package feature.step.definition;
-
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
-import net.thucydides.core.annotations.Steps;
-import ui.DatasourcePage;
-import ui.HomePage;
-
-public class CreateAnalyzerReportStepDefinition {
-
-  @Steps
-  HomePage homepage;
-  @Steps
-  DatasourcePage datasourcePage;
-
-  @Given("^Admin is o the file menu$")
-  public void admin_is_o_the_file_menu(){
-    //homepage.clinkOnfielMenu();
-    //homepage.clinkCreateNew();
-  }
-
-
-  @When("^Admin choose new Analysis report$")
-  public void admin_choose_new_Analysis_report(){
-    homepage.clickCreateNew();
-    homepage.clinkCreateNewAnalyzerReport();
-  }
-
-  @When("^Choose data source$")
-  public void choose_data_source(){
-    datasourcePage.chooseDatasource( "SteelWheels: SteelWheelsSales" );
-    datasourcePage.clickOnOkButton();
-  }
-
-  @Then("^Admin should be in analysis report page$")
-  public void admin_should_be_in_analysis_report_page(){
-  }
-}
+//package feature.step.definition;
+//
+//import cucumber.api.java.en.Given;
+//import cucumber.api.java.en.Then;
+//import cucumber.api.java.en.When;
+//import net.thucydides.core.annotations.Steps;
+//import ui.DatasourcePage;
+//import ui.HomePage;
+//
+//public class CreateAnalyzerReportStepDefinition {
+//
+//  @Steps
+//  HomePage homepage;
+//  @Steps
+//  DatasourcePage datasourcePage;
+//
+//  @Given("^Admin is o the file menu$")
+//  public void admin_is_o_the_file_menu(){
+//    //homepage.clinkOnfielMenu();
+//    //homepage.clinkCreateNew();
+//  }
+//
+//
+//  @When("^Admin choose new Analysis report$")
+//  public void admin_choose_new_Analysis_report(){
+//    homepage.clickCreateNew();
+//    homepage.clinkCreateNewAnalyzerReport();
+//  }
+//
+//  @When("^Choose data source$")
+//  public void choose_data_source(){
+//    datasourcePage.chooseDatasource( "SteelWheels: SteelWheelsSales" );
+//    datasourcePage.clickOnOkButton();
+//  }
+//
+//  @Then("^Admin should be in analysis report page$")
+//  public void admin_should_be_in_analysis_report_page(){
+//  }
+//}
